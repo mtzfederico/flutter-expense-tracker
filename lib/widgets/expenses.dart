afa454543b334a27a6294deeebc5fa9203317c0d
Federico Martinez <fede@mtzfederico.com>
2026-03-31 12:09:33 -0400

    Replaced SizedBox with Expanded

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -15,6 +15,7 @@ class _ExpensesState extends State<Expenses> {
   final List<Expense> _registeredExpenses = [
     Expense(title: "Oat Late", amount: 7.36, date: DateTime.now(), category: Category.food),
     Expense(title: "NYC Subway", amount: 3.00, date: DateTime.now(), category: Category.travel),
+    Expense(title: "Movie Ticket", amount: 18.00, date: DateTime.now(), category: Category.leisure),
   ];
 
   @override
@@ -24,8 +25,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           Text("CHART GOES HERE"),
           SizedBox(height: 30),
-          SizedBox(
-            height: 300,
+          Expanded(
             child: ExpensesList(expenses: _registeredExpenses)
           ),
         ],
