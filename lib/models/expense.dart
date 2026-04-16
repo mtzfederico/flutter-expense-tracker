0a33611ba380922c804a7b6de63b186a8afc0249
Federico Martinez <fede@mtzfederico.com>
2026-04-16 11:45:25 -0400

    Added ExpenseBucket

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
+
 const uuid = Uuid();
 
 final formatter = DateFormat('MMMM d y hh:mm a'); // April 2 2026 11:20 AM
 
-enum Category {food, travel, leisure, work}
-const categoryIcons={
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
   Category.food: Icons.lunch_dining,
   Category.travel: Icons.flight_takeoff,
   Category.leisure: Icons.beach_access,
-  Category.work: Icons.work
+  Category.work: Icons.work,
 };
 
 class Expense {
-  Expense({required this.title, required this.amount, required this.date, required this.category}) : id = uuid.v7();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v7();
 
   final String id;
   final String title;
@@ -24,5 +31,25 @@ class Expense {
 
   String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+  
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses = allExpenses
+          .where((expense) => expense.category == category)
+          .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
 }
